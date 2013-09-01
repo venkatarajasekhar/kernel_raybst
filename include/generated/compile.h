11408e165ab2a5ec8d21b6597d2a78a7b8414050
Czar Dixon <rbheromax@gmail.com>
2013-08-31 23:04:10 -0500

    minor changes

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 5 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 PREEMPT Sat Aug 31 11:08:49 CDT 2013"
+#define UTS_VERSION "#5 PREEMPT Sat Aug 31 22:02:14 CDT 2013"
 #define LINUX_COMPILE_BY "rbheromax"
 #define LINUX_COMPILE_HOST "agerthorn"
 #define LINUX_COMPILER "gcc version 4.7.3 20121106 (prerelease) (Linaro GCC 4.7-2012.11) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-rb3-gb5c9710-dirty"
+#define UTS_RELEASE "3.4.0-rb3-g032863e-dirty"
