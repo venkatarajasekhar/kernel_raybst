032863e68b5eb8b633a2aa965b483431a87be1da
Czar Dixon <rbheromax@gmail.com>
2013-08-31 21:49:42 -0500

    push

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 PREEMPT Sat Aug 31 11:08:49 CDT 2013"
+#define LINUX_COMPILE_BY "rbheromax"
+#define LINUX_COMPILE_HOST "agerthorn"
+#define LINUX_COMPILER "gcc version 4.7.3 20121106 (prerelease) (Linaro GCC 4.7-2012.11) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.0-rb3-gb5c9710-dirty"
