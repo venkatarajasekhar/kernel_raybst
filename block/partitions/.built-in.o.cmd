937bbd37e1ae29bdef0d38b7a20998777c931d04
Czar Dixon <rbheromax@gmail.com>
2013-09-13 23:37:56 -0500

    adding sio (pt 1)

---
+++ block/partitions/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_block/partitions/built-in.o :=  /home/rbheromax/toolchains/arm-eabi-4.4.3/bin/arm-eabi-ld -EL    -r -o block/partitions/built-in.o block/partitions/check.o block/partitions/msdos.o 
