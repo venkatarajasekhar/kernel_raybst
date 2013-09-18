84e0a22dff87ebba992e35a5be6f7c7cab7ec6cd
Czar Dixon <rbheromax@gmail.com>
2013-09-17 22:23:08 -0500

    drivers: add usb fastchg to kernel (thanks faux123)

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -46,6 +46,11 @@
 #include <mach/msm_bus.h>
 #include <mach/rpm-regulator.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#define USB_FASTCHG_LOAD 1000 /* 1000 uA */
+#endif
+
 #define MSM_USB_BASE	(motg->regs)
 #define DRIVER_NAME	"msm_otg"
 
@@ -1071,6 +1076,21 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 
 	if (motg->cur_power == mA)
 		return;
+
+	motg->cur_power == mA;
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge == 1) {
+			mA = USB_FASTCHG_LOAD;
+			pr_info("USB fast charging is on - 1000mA. \n");
+		}
+		else {
+			pr_info("USB fast charging is off. \n");
+
+	}
+#endif
+
+
 
 	dev_info(motg->phy.dev, "Avail curr from USB = %u\n", mA);
 
